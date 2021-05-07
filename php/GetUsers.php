e19145155a799e5500f281494a4f4e12f98f0273
Jpaolo09 <jp.cortez091@gmail.com>
2021-05-07 20:07:20 +0800

    partially done users table!

--- php/GetUsers.php
+++ php/GetUsers.php
@@ -5,25 +5,23 @@
 
     if($limit > 0)
     {
-        $sql = "SELECT * FROM employees LIMIT $limit";
+        $sql = "SELECT * FROM users LIMIT $limit";
         $result = $conn->query($sql);
 
         if($result->num_rows > 0)
         {
             while($row = $result->fetch_assoc())
             {
-                echo "<tr>
-                        <td>$row[EMP_ID]</td>
-                        <td>$row[FNAME] $row[MNAME] $row[LNAME]</td>
-                        <td>020202</td>
-                        <td>$row[MANAGER_ID]</td>
-                        <td>$row[DEPT_ID]</td>
-                        <td>$row[WORK_STATUS]</td>
+                echo " <tr>
+                        <td>$row[USER_ID]</td>
+                        <td>$row[ACC_NAME]</td>
+                        <td>$row[USERNAME]</td>
+                        <td>$row[ROLE]</td>
                         <td>
                             <div class='emp-tab-buttons text-center'>
                                 <button class='btn btn-primary text-light'> <i class='material-icons' style='font-size:16px;color:white'>info</i> view</button>
                                 <button class='btn btn-primary text-light'> <i class='material-icons' style='font-size:16px'>edit</i> edit</button>
-                                <button class='btn btn-primary text-light'> <i class='material-icons' style='font-size:16px'>print</i> print</button>
+                                <button class='btn btn-primary text-light'> <i class='material-icons' style='font-size:16px;color:white'>delete</i> delete</button>
                             </div>
                         </td>
                     </tr>";
